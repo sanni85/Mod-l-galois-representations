fc9792b061464d8994ef333b9b4ec52a1faf5dbf
John Cremona <john.cremona@gmail.com>
2020-08-19 04:15:48 -0400

    updated modell code and output for 2,3,5

--- code/mfmodell/modell_new.py
+++ code/mfmodell/modell_new.py
@@ -8,7 +8,7 @@
 # database.
 #
 
-from sage.all import ZZ, QQ, GF, PolynomialRing, NumberField, primes, Matrix, FiniteDimensionalAlgebra
+from sage.all import ZZ, QQ, GF, PolynomialRing, NumberField, primes_first_n, Matrix, FiniteDimensionalAlgebra
 from lmfdb import db
 from lmfdb.classical_modular_forms.web_newform import WebNewform
 import logging
@@ -73,7 +73,7 @@ def make_an_decoder(nf):
 # which can be expensive, even though theoretically we only need an
 # ell-maximal order.
 
-def reduction_maps(betas, ell):
+def reduction_maps(betas, ell, verbose=False):
     """Input: 
 
     betas: a list of d ZZ-module generators of an order O in a number
@@ -99,7 +99,8 @@ def reduction_maps(betas, ell):
     #print("betas = {}".format(betas))
     assert betas[0]==1
     Fl = GF(ell)
-    print("creating Hecke order mod {} from betas".format(ell))
+    if verbose:
+        print("creating Hecke order mod {} from betas".format(ell))
     coords = K.gen().coordinates_in_terms_of_powers()
     U = Matrix([coords(b) for b in betas]).inverse()
     structure = [(Matrix([coords(bi*bj) for bj in betas])*U).change_ring(Fl)
@@ -112,13 +113,36 @@ def reduction_maps(betas, ell):
     d = len(betas)
     vv = [list(M.basis_matrix().right_kernel().basis()[0])
           for M in MM  if M.basis_matrix().rank()==d-1]
-    print("{} reductions found from Hecke order".format(len(vv)))
-    return [lambda w: Fl(sum([vi*Fl(wi) for vi,wi in zip(v,w)])) for v in vv]
+
+    # Taking the dot product with each of these basis vectors gives a
+    # GF(l)-linear map from the Hecke order to GF(l), but we need it
+    # to be a ring homomorphism, so we must scale it so 1 maps to 1.
+    # In practice Sage will always scale the basis vector so that the
+    # first nonzero entry (which here must be the first entry) is 1,
+    # but we should not rely on this.
+
+    for v in vv:
+        if v[0]!=1:
+            if verbose:
+                print("Rescaling reduction vector v={}".format(v))
+            if v[0]==0:
+                raise ValueError("reduction map defined by {} maps 1 to 0".format(v))
+            v0inv = 1/v[0]
+            v = [vi*v0inv for vi in v]
+            if verbose:
+                print("Rescaled reduction vector v={}".format(v))
+    if verbose:
+        print("{} reductions found from Hecke order".format(len(vv)))
+        if vv:
+            print("Reduction vector(s):")
+            for v in vv: print(v)
+    #return [lambda w: Fl(sum([vi*Fl(wi) for vi,wi in zip(v,w)])) for v in vv]
+    return vv
 
 # Function to construct all the reuductions of one newform at a prime
 # ell:
 
-def make_reductions(nf, ell):
+def make_reductions(nf, ell, verbose=False):
     """Input: A newform nf and a prime ell
 
     Output: a list of all the associated reducton maps
@@ -129,19 +153,49 @@ def make_reductions(nf, ell):
 
     """
     K = nf.hecke_field
+    if verbose:
+        print("Making reduction mod ell={}".format(ell))
     if K==QQ:
+        # if verbose:
+        #     print("K=QQ, easy")
         Fl = GF(ell)
         return [lambda elt: Fl(QQ(elt))]
     
     assert nf.hecke_ring_cyclotomic_generator==0
 
-    if nf.hecke_ring_power_basis:
+    if not nf.hecke_ring_numerators:#nf.hecke_ring_power_basis:
         a = K.gen()
         betas = [a**i for i in range(K.degree())]
+        # if verbose:
+        #     print("power basis case, Z-basis {}".format(betas))
     else: # generic
+        # if verbose:
+        #     print("Hecke ring numerators: {}".format(nf.hecke_ring_numerators))
+        #     print("Hecke ring denominators: {}".format(nf.hecke_ring_denominators))
         betas = [K(c)/d for c,d in zip(nf.hecke_ring_numerators, nf.hecke_ring_denominators)]
-    return reduction_maps(betas, ell)
-   
+        # if verbose:
+        #     print("non-power basis case, Z-basis {}".format(betas))
+    return reduction_maps(betas, ell, verbose)
+
+def apply_red(coeffs, red):
+    """Apply a reduction map to one Hecke ring element
+
+    Input:
+
+    - coeffs: list of d integers (d>0) giving the ZZ-coefficients of
+      an element of the Hecke ring.
+
+    - red: list of d elements of GF(l) determining a ring homomorphism
+      from the Hecke ring to GF(l).
+
+    Output: an element of GF(l).
+    """
+    # When each red was implemented as a map we would just need to do
+    # return red(coeffs)
+    
+    Fl = red[0].parent()    
+    return Fl(sum([ci*ri for ci,ri in zip(coeffs,red)]))
+
 def reduce_an_mod_ell(nf,red):
     """Input: a newform and one reduction map to GF(ell) for some ell
 
@@ -149,16 +203,21 @@ def reduce_an_mod_ell(nf,red):
     q-expansion coefficients a_n of the newform.
 
     """
-    return [red(an) for an in nf.qexp]
+    return [apply_red(an,red) for an in nf.an]
     
-def reduce_ap_mod_ell(nf, red):
+def reduce_ap_mod_ell(nf, red, verbose=False):
     """Input: a newform and one reduction map to GF(ell) for some ell
 
     Output: a list of pairs (p,a_p mod ell) for the prime index
     q-expansion coefficients a_p of the newform.
 
     """
-    return dict([(p,red(nf.qexp[p])) for p in primes(nf.qexp_prec)])
+    if verbose:
+        print("Reducing {} mod ell via {}".format(nf.label, red))
+        for i,p in enumerate(primes_first_n(10)):
+            ap = nf.ap[i]
+            print("p={}, ap={}, ap mod ell = {}".format(p, ap, apply_red(ap, red)))
+    return dict([(p,apply_red(nf.ap[i], red)) for i,p in enumerate(primes_first_n(len(nf.ap)))])
 
 def reduce_chi_mod_ell(nf, red):
     """Input: a newform and one reduction map to GF(ell) for some ell
@@ -176,7 +235,7 @@ def reduce_chi_mod_ell(nf, red):
     """
     if nf.char_order==1:
         return []
-    return [[a,red(coeffs)] for a, coeffs in nf.hecke_ring_character_values]
+    return [[a,apply_red(coeffs, red)] for a, coeffs in nf.hecke_ring_character_values]
 
 ########################################################################
 #
@@ -204,6 +263,7 @@ def get_forms(N, k, ell, verbose=False):
     if N%ell==0:
         return [], []
     nfs  = db.mf_newforms
+    heckes = db.mf_hecke_nf # for more ap
     forms  = [WebNewform.by_label(f_label) for f_label in
               nfs.search({'level':N, 'weight':k}, projection='label')]
     if verbose:
@@ -230,10 +290,22 @@ def get_forms(N, k, ell, verbose=False):
         f.hecke_field = NumberField(Qx(f.field_poly), 'a_')
         if verbose:
             print("making reductions for {} mod {}".format(f.label, ell))
-        f.reductions = make_reductions(f, ell)
+            print("Hecke field is {}".format(f.hecke_field))
+        f.reductions = make_reductions(f, ell, verbose)
         if verbose:
             print("finished making reductions for {} mod {}".format(f.label, ell))
 
+        # get extra ap from the second table:
+        anap = heckes.lucky({'label':f.label}, projection=['ap','an'])
+        f.an = anap['an']
+        f.ap = anap['ap']
+
+        if verbose:
+            nap = len(f.ap)
+            print("Found {} ap and {} an in the second table".format(nap,len(f.an)))
+            # for p,ap in zip(primes_first_n(nap),f.ap):
+            #     print("p={}, ap={}".format(p,ap))
+        
     # Exclude forms with no mod-ell reductions:
         
     forms = [f for f in forms if f.reductions]
@@ -273,9 +345,10 @@ def compare_record(nf1,nf2):
     """
     return (nf1['ell'] == nf2['ell']) and (nf1['ap'] == nf2['ap'])
 
-def nf_to_string(nfr):
-    """
-    Return the formatted output string for a newform mod ell dict.
+def nf_to_string(nfr, nap=None):
+    """Return the formatted output string for a newform mod ell dict.  If
+    nap is a positive integer (and not None, the default) then only a
+    maximum of nap ap's are output.
 
     The string uses ":" as separators between the fields
 
@@ -288,30 +361,33 @@ def nf_to_string(nfr):
     - index       (index of this mod-ell newform among all with the same ap mod ell)
     - chi_mod_ell (character value list mod ell)
     - ap          (list of ap mod ell)
+
     """
     label = nfr['label']
     N,k,c,id = label.split(".")
     ell = str(nfr['ell'])
-    # old long format
-    # ap = ["[{},{}]".format(p,nfr['ap'][p]) for p in sorted(nfr['ap'].keys())]
 
     # The ap mod ell are in a single comma-separated list:
-    ap = str([nfr['ap'][p] for p in sorted(nfr['ap'].keys())]).replace(" ","")[1:-1]
+    aplist = [nfr['ap'][p] for p in sorted(nfr['ap'].keys())]
+    if nap and nap<len(aplist):
+        aplist = aplist[:nap]
+    ap = str(aplist).replace(" ","")[1:-1]
 
     index = str(nfr['index'])
     dim = str(nfr['d'])
     chi_mod_ell = str(nfr['chi_mod_ell']).replace(" ","")
     return ":".join([label, N,k,c,id, dim, ell, index, chi_mod_ell, ap])
 
-def data_output(nf_list, filename, mode='w'):
-    """
-    Output a list of full nf-mod-ell records to the given file.
+def data_output(nf_list, filename, mode='w', nap=None):
+    """Output a list of full nf-mod-ell records to the given file.  If
+    nap is a positive integer (and not None, the default) then only a
+    maximum of nap ap's are output.  NB: contents of file will be
+    over-written unless mode='a'.
 
-    NB: contents of file will be over-written unless mode='a'.
     """
     o = open(filename, mode=mode)
     for nf in nf_list:
-        o.write(nf_to_string(nf) + "\n")
+        o.write(nf_to_string(nf, nap) + "\n")
     o.close()
     print("{} forms output to {}".format(len(nf_list),filename))
 
@@ -333,6 +409,14 @@ def extra_output(nf_list, filename, mode='w'):
 ########################################################################
 #
 # Function for a systematic run over a range of levels and primes
+#
+# After running, use data_output() and extra_output() to output to suitable files
+#
+# e.g.
+# sage: res = run([1..100],[2,3,5],True)
+# sage: for ell in [2,3,5]:
+#           data_output(res[0][ell], "mod_{}_100.txt".format(ell))
+#           extra_output(res[1][ell], "mod_{}_100_missing.txt".format(ell))
 #
 ########################################################################
     
@@ -361,7 +445,7 @@ def run(levels, ells=[2,3,5], verbose=True):
             for N in levels:
                 if verbose:
                     print("(ell,k,N) = ({},{},{})".format(ell,k,N))
-                ff, ff_no_field = get_forms(N,k,ell)
+                ff, ff_no_field = get_forms(N,k,ell, verbose=verbose)
                 if ff_no_field:
                     if verbose:
                         print("No Hecke field exists for {}".format(ff_no_field))
@@ -390,8 +474,6 @@ def run(levels, ells=[2,3,5], verbose=True):
                 print("Unable to reduce {} mod {} (dimension={})".format(f[0],f[2],f[1]))
     return nf, nfx
 
-# After running the above use data_output() to output nf[ell] for each ell to a suitable file, and extra_output() to output nfx[ell]
-
 def compare_new_old(new_f, old_f):
     # compare levels
     if old_f['N'] != new_f['N']:
@@ -401,3 +483,15 @@ def compare_new_old(new_f, old_f):
             return False
     return True
 
+"""
+To extract the dimensions from an "extra" output file:
+awk -F ":" '{print $2;}'  mod_5_100_missing.txt | sort -n | uniq
+
+To count the distinct mod-ell reductions in a normal output file:
+awk -F ":" '$8==1{print $8;}' mod_5_100.txt | wc -l
+
+To see the largest multiplicities:
+awk -F ":" '$8>1{print $8;}' mod_2_100.txt | sort -n | uniq | tail
+
+
+"""
