f233355cfcbd3abe6a6ffcd15100cb8a2ad56de1
John Cremona <john.cremona@gmail.com>
2024-08-30 14:56:06 +0100

    updated code/mfmod and data/mfmod and MANIFEST

--- MANIFEST.md
+++ MANIFEST.md
@@ -18,26 +18,55 @@ Contains data output from modell.py: data/mfmodell/<ell> for ell=0,2,3,5,7 conta
 ell=2
 
 - data/mfmodell/2/1-1000.txt contains 12739 mod 2 forms from levels 1-1000 (1960 excluding repeats)
-- data/mfmodell/2/x1-1000.txt contains 1368 undecomposed spaces of dim>50, not processed for mod 2 forms
-- data/mfmodell/2/mod2bb1000.txt contains BlackBox output for 9609 mod 2 reductions (includes repeats)
-    one line each saying "reducible" or "S3" or "C3" with cubics defining splitting fields, e.g.
+  format: one per line, fields separated by :, listed in code/mfmodell/modell.py's nf_to_string().
+  NB if a newform has several different mod-ell reductions this is not recorded in the labelling in this file
 
-    5.4.a.a:reducible
-    11.2.a.a:S3:x^3-x^2-x-1
-    49.4.a.c:C3:x^3-7*x-7
+- data/mfmodell/2/x1-1000.txt contains 1368 undecomposed spaces of dim>50, not processed for mod 2 forms
+- data/mfmodell/2/mod2bb1000.txt contains BlackBox output for 12739 mod 2 reductions:
+
+    - one line each with fields separated by ":".
+    - First field is the modular form label with additional "-1", "-2",
+      etc to distinguish multiple reductions of the same modular form.
+    - Second field is "reducible" or "S3" or "C3".
+    - Third field (for irreducibles) is a cubic defining the splitting field.
+
+    e.g.
+
+    5.4.a.a-1:reducible
+    11.2.a.a-1:S3:x^3-x^2-x-1
+    49.4.a.c-1:C3:x^3-7*x-7
+
+- data/mfmodell/2/mod2dim2reps.yxy is a dump from the database modlgal table of rows with ell=2 and dim=2.
+- data/mfmodell/2/label_map_2.txt has one line for each database entry
+  for ell=2 showing which modular forms match it (a_p for good p<100
+  and splitting fields match).
+- data/mfmodell/2/reverse_label_map_2.txt: inverse of previous.
+
 
 ell=3
 
 - data/mfmodell/3/1-1000.txt contains 14293 mod 3 forms from levels 1-1000 (8008 excluding repeats)
 - data/mfmodell/3/x1-1000.txt contains  2254 undecomposed spaces of dim>50, not processed for mod 3 forms
-- data/mfmodell/3/mod3bb1000nr.txt contains BlackBox output for 1550 mod 3 reductions (excludes repeats)
-   one line each saying "reducible" or linear and projective image if
-   irreducible with polynomials defining splitting fields, e.g.
+- data/mfmodell/3/mod3bb1000.txt contains BlackBox output for 24569 mod 3 reductions:
 
-    5.4.a.a:GL(2,3):x^8-4*x^7+7*x^6-7*x^5+4*x^4-x^3-4*x^2+4*x-1:S4:x^4-x^3+4*x-1
-    7.3.b.a:Ns:(x^4-x^3-3*x^2-x+1)*(x^4-x^3+2*x+1):V4:(x^2-21)*(x^2+3)
-    7.4.a.a:GL(2,3):x^8-6*x^4-x^2-3:S4:x^4-x^3-3*x^2-7*x+1
-    8.3.d.a:reducible
+    - one line each with fields separated by ":".
+    - First field is the modular form label with additional "-1", "-2",
+      etc to distinguish multiple reductions of the same modular form.
+    - 2nd field is "reducible" or "GL(2,3)" or "Nn" or "Ns" (image).
+    - 3rd field (for irreducibles) is an octic defining the splitting field.
+    - 4th field (for irreducibles) is "S4" or "V4" or "D4" (projective image)
+    - 5th field (for irreducibles) is a quartic defining the projective splitting field.
+
+    5.4.a.a-1:GL(2,3):x^8-4*x^7+7*x^6-7*x^5+4*x^4-x^3-4*x^2+4*x-1:S4:x^4-x^3+4*x-1
+    7.3.b.a-1:Ns:(x^4-x^3-3*x^2-x+1)*(x^4-x^3+2*x+1):V4:(x^2-21)*(x^2+3)
+    7.4.a.a-1:GL(2,3):x^8-x^7-3*x^6+2*x^5+4*x^4+3*x^3-5*x^2-7*x-3:S4:x^4-x^3-3*x^2-7*x+1
+    8.3.d.a-1:reducible
+
+- data/mfmodell/3/mod3dim2reps.yxy is a dump from the database modlgal table of rows with ell=3 and dim=2.
+- data/mfmodell/3/label_map_3.txt has one line for each database entry
+  for ell=3 showing which modular forms match it (a_p for good p<100
+  and splitting fields and projective splitting fields match).
+- data/mfmodell/3/reverse_label_map_3.txt: inverse of previous.
 
 ell=5
 
--- code/mfmodell/match_labels.py
+++ code/mfmodell/match_labels.py
@@ -36,6 +36,7 @@ def read_database_dump(filename):
     return data
 
 def read_mfmod_data(filename, nap=None):
+    # Use to read the mf reduction data from the files 1-1000.txt & similar
     data = {}
     label_counter = {}
     with open(filename) as input:
@@ -53,10 +54,68 @@ def read_mfmod_data(filename, nap=None):
             label_counter[label] = i
             xlabel = "-".join([label, str(i)])
             data[xlabel] = {'ell':ell, 'label':label, 'xlabel':xlabel, 'ap': ap}
-    # later we'll also read the kpol and pkpol from the files mod*bb*
     return data
 
-def compare(data1, data2):
+ZZx = PolynomialRing(ZZ, 'x')
+
+def parse_poly_string(pol):
+    """
+    Given a polynomial in ZZ[x], return the list of its coefficients.
+    """
+    return ZZx(pol).coefficients(sparse=False)
+
+def read_mfmod_bbdata(filename, ell, ignore_reducibles=True):
+    """
+    Use to read the kpol and pkpol from the files mod[23]bb*.  If
+    ignore_reducibles is True, only the irreducible ones are returned.
+
+    For ell=2 the format of each line is one of
+
+    label:reducible
+    label:image_name:kernel_poly
+
+    where image_name is 'S3' or 'C3' and 'kernel_poly' is a polynomial in x as a string e.g. 'x^3-x^2-x-1'.
+
+    For ell=3 the format of each line is one of
+
+    label:reducible
+    label:image_name:kernel_poly:projective_image_name:projective_kernel_poly
+
+    where image_name is 'GL(2,3)' or 'Nn' or 'Ns', projective_image_name is (repectively) 'S4', D4, V4';
+    and 'kernel_poly', 'projective_kernel_poly' are polynomial strings e.g. 'x^3-x^2-x-1'.
+
+    """
+    if ell not in [2,3]:
+        raise RuntimError(f"BBdata only for ell=2, 3, not {ell}")
+    data = {}
+    with open(filename) as input:
+        for L in input:
+            L = L.strip()
+            line_data = L.split(":")
+            label = line_data[0]
+            image = line_data[1]
+            if ell==2:
+                if len(line_data)>2:
+                    kpol = parse_poly_string(line_data[2])
+                else:
+                    kpol = None
+                if not (image == 'reducible' and ignore_reducibles):
+                    data[label] = {'ell':ell, 'label':label,
+                                   'image':image, 'kpol': kpol}
+            if ell==3:
+                if len(line_data)>2:
+                    kpol = parse_poly_string(line_data[2])
+                    proj_image = line_data[3]
+                    pkpol = parse_poly_string(line_data[4])
+                else:
+                    kpol = pkpol = proj_image = None
+                if not (image == 'reducible' and ignore_reducibles):
+                    data[label] = {'ell':ell, 'label':label,
+                                   'image':image, 'kpol': kpol,
+                                   'proj_image':proj_image, 'pkpol': pkpol}
+    return data
+
+def compare_ap(data1, data2):
     """
     Return True if
     (1) data1['ell']==data2['ell']
@@ -69,6 +128,82 @@ def compare(data1, data2):
             if ap != data2['ap'][p]:
                 return False
     return True
+
+# dict with keys pairs of polynomials (as coefficient sequences), values True iff splitting fields are isomorphic
+splitting_field_dict = {}
+
+def equal_splitting_fields(pol1, pol2):
+    global splitting_field_dict
+    k = (tuple(pol1), tuple(pol2))
+    if k in splitting_field_dict:
+        return splitting_field_dict[k]
+    K1 = ZZx(pol1).splitting_field('a1')
+    K2 = ZZx(pol2).splitting_field('a2')
+    res = K1.is_isomorphic(K2)
+    splitting_field_dict[k] = res
+    return res
+
+def compare_fields(data1, data2):
+    """
+    Return True if
+    (1) data1['ell']==data2['ell']
+    (2) data1['kpol']==data2['kpol']
+        or they have the same splitting field
+    (3) [ell=3 only] data1['pkpol']==data2['pkpol']
+        or they have the same splitting field
+
+    """
+    ell = data1['ell']
+    #print(f"comparing {data1} and {data2} which have compatible ap mod {ell}")
+
+    #return ell == data2['ell'] and data1['kpol'] == data2['kpol'] and (ell==2 or data1['pkpol'] == data2['pkpol'])
+
+    if ell!=data1['ell']:
+        return False
+    if data1['kpol'] != data2['kpol']:
+        print(f"{data1['label']} kpol {data1['kpol']} and {data2['label']} kpol {data2['kpol']} differ!")
+        print(f"image = {data2['image']} - checking splitting fields")
+        if not equal_splitting_fields(data1['kpol'], data2['kpol']):
+            print(f"{data1['label']} and {data2['label']}: database and BB have non-isomorphic splitting fields")
+            return False
+        else:
+            print("OK, splitting fields agree")
+    if ell==2:
+        return True
+    if data1['pkpol'] != data2['pkpol']:
+        print(f"{data1['label']} pkpol {data1['pkpol']} and {data1['label']} pkpol {data2['pkpol']} differ!")
+        print(f"proj.image = {data2['proj_image']} - checking splitting fields")
+        if not equal_splitting_fields(data1['pkpol'], data2['pkpol']):
+            print(f"{data1['label']} and {data2['label']}: database and BB have non-isomorphic projective splitting fields")
+            return False
+        else:
+            print("OK, projective splitting fields agree")
+    return True
+
+def sort_key_modlgal(label):
+    """
+    Label is ell.d.N.n with all fields numeric
+    """
+    return [ZZ(x) for x in label.split(".")]
+
+def sort_key_modform(label):
+    """
+    Label is N.k.x.y with N,k numeric, x and y alphabetic
+    """
+    t = label.split(".")
+    for i in range(2):
+        t[i] = ZZ(t[i])
+    return t
+
+def sort_key_modformx(label):
+    """
+    Label is N.k.x.y-i with N,k,i numeric, x and y alphabetic
+    """
+    t = label.split(".")
+    t = t[:3] + t[3].split("-")
+    for i in [0,1,4]:
+        t[i] = ZZ(t[i])
+    return t
 
 def match_labels(file1, file2, outputfile, reverse=False):
     """
@@ -89,9 +224,62 @@ def match_labels(file1, file2, outputfile, reverse=False):
     if reverse:
         data1, data2 = data2, data1
     n = 0
+    outdata = {}
+    for l1, m1 in data1.items():
+        matches = [l2 for l2, m2 in data2.items() if compare_ap(m1,m2)]
+        matches.sort(key = sort_key_modlgal if reverse else sort_key_modformx)
+        outdata[l1] = matches
+
+    keys = list(outdata.keys())
+    keys.sort(key = sort_key_modformx if reverse else sort_key_modlgal)
     with open(outputfile, 'w') as out:
-        for l1, m1 in data1.items():
-            matches = [l2 for l2, m2 in data2.items() if compare(m1,m2)]
-            out.write(f"{l1}:{','.join(matches)}\n")
+        for k in keys:
+            out.write(f"{k}:{','.join(outdata[k])}\n")
             n += 1
+
+    print(f"{n} lines written to {outputfile}")
+
+def match_labels_and_fields(ell, file1, file2, file3, outputfile):
+    """
+    ell is 2 or 3
+    file1 contains a database dump for some ell
+    file2 contains the modular form reductions modulo ell
+    file3 contains the BB data from the modular form reductions modulo ell
+
+    Outputfile will contain one line for each label in
+    file1. Each line has the label from file1, then ':', then a
+    comma-separated list of labels from file 2 which match it.
+
+    """
+    if ell not in [2,3]:
+        raise RuntimError(f"BBdata only for ell=2, 3, not {ell}")
+    data1 = read_database_dump(file1)
+    print(f"Read {len(data1)} items from {file1}")
+    data2 = read_mfmod_data(file2)
+    print(f"Read {len(data2)} items from {file2}")
+    data3 = read_mfmod_bbdata(file3, ell) # ignores reducibles
+    print(f"Read {len(data3)} items from {file3}")
+    n = 0
+    outdata = {}
+    for l1, m1 in data1.items():
+        matches = [l2 for l2, m2 in data2.items() if compare_ap(m1,m2)]
+        field_matches = [l2 for l2 in matches if l2 in data3 and compare_fields(m1,data3[l2])]
+        matches.sort(key = sort_key_modformx)
+        field_matches.sort(key = sort_key_modformx)
+        # if matches:
+        #     print(f"{l1} matches {matches} on ap")
+        #     print(f"{l1} matches {field_matches} on splitting fields")
+        # else:
+        #     print(f"{l1} matches nothing on ap")
+        if matches != field_matches:
+            print(f"{l1} matches {matches} on ap but only {field_matches} on splitting fields")
+        outdata[l1] = field_matches
+
+    keys = list(outdata.keys())
+    keys.sort(key = sort_key_modlgal)
+    with open(outputfile, 'w') as out:
+        for k in keys:
+            out.write(f"{k}:{','.join(outdata[k])}\n")
+            n += 1
+
     print(f"{n} lines written to {outputfile}")
